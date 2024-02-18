635cc0ea526353934bfdeb0bac7ee838f3e850a7
kennynahh <kennyulna@gmail.com>
2024-02-17 23:59:17 -0500

    new file:   default.js 	modified:   website/index.html

---
+++ default.js
@@ -0,0 +1,36 @@
+// Ensure that the DOM is fully loaded before running the script
+document.addEventListener('DOMContentLoaded', function () {
+    var ctx = document.getElementById('sleepChart').getContext('2d');
+    var sleepChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ['Total Sleep', 'Awake'],
+            datasets: [{
+                label: 'Sleep Duration',
+                data: [454, 23], // Assuming 454 minutes of sleep (7hrs 34m) and 23 minutes awake
+                backgroundColor: [
+                    'rgba(131, 136, 235, 1)', // Blue shade for sleep
+                    'rgba(208, 172, 102, 1)'  // Gold shade for awake
+                ],
+                borderColor: [
+                    'rgba(255, 255, 255, 1)', // White borders
+                    'rgba(255, 255, 255, 1)'
+                ],
+                borderWidth: 2
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            circumference: Math.PI, // To make it a semi-circle (180 degrees)
+            rotation: -Math.PI, // To start from the top
+            cutoutPercentage: 60, // To increase the thickness of the doughnut
+            legend: {
+                display: false // Assuming we don't want to display the legend
+            },
+            tooltips: {
+                enabled: false // Assuming we don't want tooltips
+            }
+        }
+    });
+});
